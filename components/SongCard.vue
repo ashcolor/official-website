30369e00ee445624de267ac22db96e269d17b32e
ashcolor <s06ash27@ybb.ne.jp>
2020-03-21 19:37:59 +0900

    全体

--- components/SongCard.vue
+++ components/SongCard.vue
@@ -1,41 +1,89 @@
 <template>
-  <v-card class="d-inline-block ma-6">
-    <v-img :src="song.thumbnail | thumbnailUrl" width="200px" height="200px"></v-img>
-
-    <v-card-subtitle class="pb-0">{{song.published | dateFormat}}</v-card-subtitle>
-
-    <v-card-text class="text--primary">
+  <v-card class="d-inline-block ma-6" style="position: relative">
+    <div class="singer-icons">
+      <v-btn v-for="singer in song.singers" :key="singer" x-small :color="singer" dark>{{singer}}</v-btn>
+    </div>
+    <div class="album-icons">
+      <v-btn
+        v-for="album in song.albums"
+        :key="album"
+        x-small
+        color="primary"
+        dark
+      >{{albumTitle[album]}}</v-btn>
+    </div>
+    <v-card outlined>
+      <v-img :src="thumbnailBaseurl + song.thumbnail" width="200px" height="200px"></v-img>
+    </v-card>
+    <v-card-subtitle class="pa-2 pb-0">{{song.published | dateFormat}}</v-card-subtitle>
+    <v-card-text class="text--primary pa-2 pt-0">
       <div>{{song.name}}</div>
-      <singer-btn :singer="song.singer"></singer-btn>
     </v-card-text>
-    <v-footer>
-      <a v-if="song.youtube" :href="song.youtube" class="mr-2" target="_blank">
-        <img src="/img/icon/youtube.png" alt="YouTube" class="icon" />
-      </a>
-      <a v-if="song.niconico" :href="song.youtube" target="_blank">
-        <img src="/img/icon/nicovideo.png" alt="niconico" class="icon" />
-      </a>
+    <v-footer class="px-2">
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on }">
+          <v-btn
+            v-if="song.youtube"
+            :href="song.youtube"
+            target="_blank"
+            v-on="on"
+            class="pr-2"
+            icon
+          >
+            <v-icon>mdi-youtube</v-icon>
+          </v-btn>
+        </template>
+        <span>YouTube</span>
+      </v-tooltip>
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on }">
+          <v-btn
+            v-if="song.niconico"
+            :href="song.niconico"
+            target="_blank"
+            v-on="on"
+            class="pr-2"
+            icon
+          >
+            <v-icon>mdi-television-classic</v-icon>
+          </v-btn>
+        </template>
+        <span>niconico</span>
+      </v-tooltip>
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on }">
+          <v-btn v-if="song.lyric" :href="song.lyric" target="_blank" v-on="on" class="pr-2" icon>
+            <v-icon>mdi-text-box</v-icon>
+          </v-btn>
+        </template>
+        <span>歌詞</span>
+      </v-tooltip>
     </v-footer>
   </v-card>
 </template>
 
 <script>
-import SingerBtn from "~/components/SingerBtn";
+import "@mdi/font/css/materialdesignicons.css";
 
 export default {
-  components: {
-    SingerBtn
-  },
   filters: {
-    thumbnailUrl: function(value) {
-      return `https://storage.googleapis.com/ashcolor-resource/thumbnail/${value}`;
-    },
     dateFormat: function(value) {
       const date = new Date(value);
       return (
         date.getFullYear() + "/" + (date.getMonth() + 1) + "/" + date.getDate()
       );
     }
+  },
+  data() {
+    return {
+      thumbnailBaseurl:
+        "https://storage.googleapis.com/ashcolor-resource/thumbnail/",
+      lyricBaseurl: "https://storage.googleapis.com/ashcolor-resource/lyric/",
+      albumTitle: {
+        an_introduction_to_odds_and_ends: "ガラクタ概論",
+        song_of_the_beginning: "はじまりの歌"
+      }
+    };
   },
   props: {
     song: Object
@@ -48,6 +96,16 @@ img.icon {
   width: 20px;
   height: 20px;
 }
-</style>
-
+.singer-icons,
+.album-icons {
+  z-index: 2;
+  position: absolute;
+  right: 4px;
+}
+.singer-icons {
+  top: 2px;
+}
+.album-icons {
+  top: 28px;
+}
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,28 @@
 <template>
-  <v-layout column justify-center align-center>Coming Soon</v-layout>
+  <v-layout column justify-center align-center>
+    <h2 class="display-1 font-weight-light mb-4">new song</h2>
+    <iframe
+      width="560"
+      height="315"
+      src="https://www.youtube.com/embed/pCqN81a7e-Y?controls=0"
+      frameborder="0"
+      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+    ></iframe>
+    <nuxt-link to="/producer/music" class="font-weight-light">and more</nuxt-link>
+    <h2 class="display-1 font-weight-light mt-4 mb-4">new album</h2>
+    <iframe
+      width="315"
+      height="315"
+      scrolling="no"
+      frameborder="no"
+      allow="autoplay"
+      src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/520644207&color=%23bcbcbc&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+    ></iframe>
+    <nuxt-link to="/producer/discography" class="font-weight-light">and more</nuxt-link>
+  </v-layout>
 </template>
 
 <script>
 export default {};
 </script>
-
-
-
-<style scoped>
-</style>
