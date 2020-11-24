76ea3763411fe0cdeda3f206460d4856683c467b
ashcolor <ashcolor27@gmail.com>
2020-11-24 19:47:11 +0900

    chg: 曲更新

--- pages/gamer/bbp.vue
+++ pages/gamer/bbp.vue
@@ -1,49 +1,58 @@
 <template>
   <v-layout column justify-center align-center>
     <h2 class="display-1 font-weight-light mb-4">大合奏！バンドブラザーズP</h2>
-    <h3>配信曲</h3>
-    <a
-      v-for="contributer in contributors"
-      v-bind:key="contributer.name"
-      :href="contributer.url"
-      target="_blank"
-      class="mx-2"
-      >{{ contributer.name }}</a
-    >
-    <h3 class="mt-4">自作曲</h3>
+
+    <h3 class="mt-4">耳コピ</h3>
     <v-data-table
       :headers="headers"
       :items="bbps"
-      hide-default-footer
+      :page.sync="page"
+      :search="search"
+      items-per-page="10"
       class="mx-auto"
       max-width="400"
       style="verticle-align: middle;"
+      hide-default-footer
+      @page-count="pageCount = $event"
     >
-      <template v-slot:item.url="{ item }">
+      <template v-slot:top>
+        <v-text-field v-model="search" label="検索" class="mx-4"></v-text-field>
+      </template>
+      <template v-slot:[`item.url`]="{ item }">
         <audio :src="item.url" controls>
           <p>音声を再生するには、audioタグをサポートしたブラウザが必要です。</p>
         </audio>
       </template>
-      <template v-slot:item.genre="{ item }">
+      <template v-slot:[`item.genre`]="{ item }">
         <v-chip color="ashcolor" dark>{{ item.genre }}</v-chip>
       </template>
     </v-data-table>
+    <div class="text-center pt-2">
+      <v-pagination v-model="page" :length="pageCount"></v-pagination>
+    </div>
+
     <h3 class="mt-4">オリジナル曲</h3>
     <v-card
-      v-for="original in originals"
-      v-bind:key="original.url"
-      class="d-inline-block ma-8"
-      height="270"
-      tile
+      class="d-flex justify-center flex-wrap"
+      style="border:none"
+      outlined
     >
-      <iframe
-        width="480"
+      <v-card
+        v-for="original in originals"
+        v-bind:key="original.url"
+        class="d-inline-block ma-8"
         height="270"
-        :src="original.url"
-        frameborder="0"
-        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen
-      ></iframe>
+        tile
+      >
+        <iframe
+          width="480"
+          height="270"
+          :src="original.url"
+          frameborder="0"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+      </v-card>
     </v-card>
   </v-layout>
 </template>
@@ -56,6 +65,9 @@ export default {
   layout: "gamer",
   data() {
     return {
+      search: "",
+      page: 1,
+      pageCount: 0,
       headers: [
         {
           text: "ジャンル",
@@ -65,24 +77,8 @@ export default {
         { text: "曲名", value: "name" },
         { text: "アーティスト", value: "artist" },
         { text: "更新日", value: "modified" },
+        { text: "配信日時", value: "published" },
         { text: "", value: "url" }
-      ],
-      contributors: [
-        {
-          name: "あっしゅからー",
-          url:
-            "http://9129suppon.com/contributor/%E3%81%82%E3%81%A3%E3%81%97%E3%82%85%E3%81%8B%E3%82%89%E3%83%BC"
-        },
-        {
-          name: "あっしゅん",
-          url:
-            "http://9129suppon.com/contributor/%E3%81%82%E3%81%A3%E3%81%97%E3%82%85%E3%82%93/"
-        },
-        {
-          name: "ハーモニカ",
-          url:
-            "http://9129suppon.com/contributor/%E3%83%8F%E3%83%BC%E3%83%A2%E3%83%8B%E3%82%AB"
-        }
       ],
       originals: [
         { url: "https://www.youtube.com/embed/4qk9XSRv56E" },
