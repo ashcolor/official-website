82d82b3e7238081dd7fd0c89a0d636cca8eb9413
ashcolor <ashcolor27@gmail.com>
2021-02-20 20:43:47 +0900

    add: techtag一覧の表示

---
+++ components/engineer/TechTag.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-chip class="ma-1" :[getSizeClass(tag.count)]="true">
+    {{ tag.name }}
+  </v-chip>
+</template>
+
+<script>
+export default {
+  props: {
+    tag: Object
+  },
+  methods: {
+    getSizeClass(count) {
+      if (count >= 3) {
+        return "large";
+      } else if (count >= 2) {
+        return "default";
+      } else {
+        return "small";
+      }
+    }
+  }
+};
+</script>
--- pages/engineer.vue
+++ pages/engineer.vue
@@ -1,64 +1,71 @@
 <template>
-  <v-card class="d-flex align-content-start flex-wrap" height="100%">
-    <v-row>
-      <v-card class="flex-grow-1 flex-shrink-1">
-        <profile></profile>
-        <v-fab-transition>
-          <v-btn class="v-btn__back ma-8" color="white" to="/welcome" fab bottom left>
-            <v-icon color="primary">fa-arrow-left</v-icon>
-          </v-btn>
-        </v-fab-transition>
+  <v-card
+    class="d-flex d-flex-row flex-sm-nowrap flex-wrap align-content-start"
+    height="100%"
+  >
+    <v-card id="left-container" class="flex-grow-1 flex-shrink-0">
+      <profile></profile>
+      <v-fab-transition>
+        <v-btn
+          class="v-btn__back ma-8"
+          color="white"
+          to="/welcome"
+          fab
+          bottom
+          left
+        >
+          <v-icon color="primary">fa-arrow-left</v-icon>
+        </v-btn>
+      </v-fab-transition>
+    </v-card>
+
+    <v-card class="mx-auto pa-12 flex-grow-1 flex-shrink-1" flat>
+      <v-card height="90%">
+        <v-toolbar color="#211F1F" dense dark>
+          <span class="title">Github</span>
+        </v-toolbar>
+
+        <v-list>
+          <template v-for="(repo, index) in githubRepos">
+            <tech-list
+              :key="repo.id"
+              :count="repo.stargazers_count"
+              :tags="repo.topics"
+              :name="repo.name"
+              :description="repo.description"
+              :href="repo.html_url"
+              target="_blank"
+            />
+            <v-divider
+              :key="repo.title"
+              v-if="index !== githubRepos.length - 1"
+            ></v-divider>
+          </template>
+        </v-list>
+        <v-toolbar color="#55C500" dense dark>
+          <span class="title">Qiita</span>
+        </v-toolbar>
+        <v-list>
+          <template v-for="(item, index) in qiitaItems">
+            <tech-list
+              :key="item.id"
+              :count="item.likes_count"
+              :tags="item.tags"
+              :name="item.title"
+              :href="item.html_url"
+              target="_blank"
+            />
+            <v-divider
+              :key="item.title"
+              v-if="index !== qiitaItems.length - 1"
+            ></v-divider>
+          </template>
+        </v-list>
       </v-card>
-      <v-card class="mx-auto pa-12 flex-grow-1 flex-shrink-1" flat>
-        <v-card>
-          <v-toolbar color="#211F1F" dense dark>
-            <span class="title">Github</span>
-          </v-toolbar>
-          <v-list>
-            <template v-for="(repo, index) in githubRepos">
-              <v-list-item :key="repo.id" :href="repo.html_url" target="_blank">
-                <v-list-item-icon>
-                  <v-icon>mdi-star</v-icon>
-                  {{ repo.stargazers_count }}
-                </v-list-item-icon>
-                <v-list-item-content>
-                  <v-chip v-for="topic in repo.topics" :key="topic" x-small>
-                    {{
-                    topic
-                    }}
-                  </v-chip>
-                  <v-list-item-title v-text="repo.name"></v-list-item-title>
-                  <v-list-item-subtitle v-text="repo.description"></v-list-item-subtitle>
-                </v-list-item-content>
-              </v-list-item>
-              <v-divider :key="repo.title" v-if="index !== githubRepos.length - 1"></v-divider>
-            </template>
-          </v-list>
-          <v-toolbar color="#55C500" dense dark>
-            <span class="title">Qiita</span>
-          </v-toolbar>
-          <v-list>
-            <template v-for="(item, index) in qiitaItems">
-              <v-list-item :key="item.id">
-                <v-list-item-icon width="200">
-                  <v-icon>mdi-star</v-icon>
-                  {{ item.likes_count }}
-                </v-list-item-icon>
-                <v-list-item-content>
-                  <v-chip v-for="tag in item.tags" :key="tag.name" x-small>
-                    {{
-                    tag.name
-                    }}
-                  </v-chip>
-                  <v-list-item-title v-text="item.title"></v-list-item-title>
-                </v-list-item-content>
-              </v-list-item>
-              <v-divider :key="item.title" v-if="index !== qiitaItems.length - 1"></v-divider>
-            </template>
-          </v-list>
-        </v-card>
-      </v-card>
-    </v-row>
+      <v-footer>
+        <tech-tag :tag="tag" v-for="tag in tags" :key="tag.name" />
+      </v-footer>
+    </v-card>
   </v-card>
 </template>
 
@@ -66,17 +73,47 @@
 import axios from "axios";
 import "@fortawesome/fontawesome-free/css/all.css";
 import Profile from "~/components/engineer/Profile.vue";
+import TechList from "../components/engineer/TechList.vue";
+import TechTag from "../components/engineer/TechTag.vue";
 
 export default {
   layout: "engineer",
   components: {
-    Profile
+    Profile,
+    TechTag,
+    TechList
   },
   data() {
     return {
       githubRepos: [],
       qiitaItems: []
     };
+  },
+  computed: {
+    tags: function() {
+      let tags = [];
+      this.githubRepos.forEach(repo => {
+        tags = tags.concat(repo.topics);
+      });
+      this.qiitaItems.forEach(item => {
+        tags = tags.concat(item.tags);
+      });
+
+      let uniqTags = Array.from(new Set(tags));
+      let tagCounts = [];
+      uniqTags.forEach(value => {
+        tagCounts.push({
+          name: value,
+          count: tags.filter(tag => {
+            return tag == value;
+          }).length
+        });
+      });
+
+      tagCounts = tagCounts.sort((a, b) => b["count"] - a["count"]);
+
+      return tagCounts;
+    }
   },
   created: function() {
     this.getGithub(), this.getQiita();
@@ -87,19 +124,26 @@ export default {
         "https://asia-northeast1-official-website-271208.cloudfunctions.net/github-repos"
       );
       if (response.status !== 200) return;
-      this.githubRepos = response.data.sort(
+      let repos = response.data.sort(
         (a, b) => b["stargazers_count"] - a["stargazers_count"]
       );
-      this.githubRepos = this.githubRepos.filter(repo => repo.fork === false);
+      repos = repos.filter(repo => repo.fork === false);
+      this.githubRepos = repos;
     },
     getQiita: async function() {
       const response = await axios.get(
         "https://asia-northeast1-official-website-271208.cloudfunctions.net/qiita-items"
       );
       if (response.status !== 200) return;
-      this.qiitaItems = response.data.sort(
+      let items = response.data.sort(
         (a, b) => b["likes_count"] - a["likes_count"]
       );
+      items = items.map(item => {
+        console.log(item);
+        item.tags = item.tags.map(tag => tag.name);
+        return item;
+      });
+      this.qiitaItems = items;
     }
   }
 };
@@ -118,27 +162,6 @@ export default {
   overflow-y: scroll;
   -ms-overflow-style: none;
   scrollbar-width: none;
-}
-.v-list::-webkit-scrollbar {
-  display: none;
-}
-.v-list-item__icon {
-  width: 60px;
-}
-.v-list-item__content {
-  flex: initial;
-  display: inline;
-}
-.v-list-item__content > * {
-  flex: initial;
-}
-.v-list-item__icon {
-  margin: auto 10px auto 0 !important;
-}
-.row,
-.col {
-  height: 100%;
-  width: 100%;
 }
 .v-btn__back {
   bottom: 0;
