de7b3afaa8282b8b639faa6140f399efb6a614c4
ashcolor <ashcolor27@gmail.com>
2022-11-03 19:26:46 +0900

    add: 各種ライブラリの導入

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,16 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   ssr: false,
+  css: ["@/assets/css/tailwind.css"],
+  build: {
+    postcss: {
+      postcssOptions: {
+        plugins: {
+          tailwindcss: {},
+          autoprefixer: {},
+        },
+      },
+    },
+  },
+  buildModules: ["@pinia/nuxt"],
 });
