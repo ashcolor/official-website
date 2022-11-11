f1f7956417a022e4a3ec1eb9d382eb8b96cfa4d4
ashcolor <ashcolor27@gmail.com>
2022-11-11 20:35:12 +0900

    add: welcomeページ作成

---
+++ components/pages/welcome/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+    <footer class="footer footer-center p-2 bg-primary text-primary-content">
+        <div>
+            <p>Copyright © 2022 ashcolor - All right reserved</p>
+        </div>
+    </footer>
+</template>
---
+++ pages/welcome.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="w-screen h-screen flex flex-col">
+        <div class="grid w-full h-full">
+            <div class="col-start-1 col-end-2 row-start-1 row-end-2">
+                <div class="h-full flex flex-col justify-center place-items-center">
+                    <h1 class="text-4xl font-thin text-center">ashcolor official website</h1>
+                    <h2 class="text-md text-primary text-center font-thin">
+                        <NuxtLink to="https://ashcolor.github.io" target="_blank">
+                            Engineer
+                        </NuxtLink>
+                        /
+                        <NuxtLink to="/" target="_blank">Producer</NuxtLink>
+                        /
+                        <NuxtLink to="/gamer" target="_blank">Gamer</NuxtLink>
+                    </h2>
+                </div>
+            </div>
+            <div class="col-start-1 col-end-2 row-start-1 row-end-2 z-[-1]">
+                <div class="h-full">
+                    <WelcomeWaveCanvas waveColorString="#9e9e9e" />
+                </div>
+            </div>
+        </div>
+        <WelcomeFooter />
+    </div>
+</template>
