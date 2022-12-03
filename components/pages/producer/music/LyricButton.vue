19e349a3f5314b5472a4ef9d48de85227070d021
ashcolor <ashcolor27@gmail.com>
2022-12-03 22:28:36 +0900

    chg: 歌詞モーダルデザイン修正

--- components/pages/producer/music/LyricButton.vue
+++ components/pages/producer/music/LyricButton.vue
@@ -41,18 +41,21 @@ const getFileFromStorage = async () => {
     </div>
     <Teleport to="body">
         <div class="modal" :class="{ 'modal-open': isShow }">
-            <div class="modal-box relative" v-on-click-outside="closeModal">
-                <label
-                    for="modal"
-                    class="btn btn-sm btn-circle absolute right-2 top-2"
-                    @click="closeModal()"
-                >
-                    ✕
-                </label>
-                <h3 class="text-lg font-bold">
-                    {{ title }}
-                </h3>
-                <p v-html="content" class="py-4"></p>
+            <div
+                class="modal-box h-full overflow-hidden flex flex-col gap-2"
+                v-on-click-outside="closeModal"
+            >
+                <div class="bg-primary text-primary-content p-4 flex justify-between">
+                    <h3 class="text-lg font-bold">
+                        {{ title }}
+                    </h3>
+                    <button class="btn btn-primary btn-sm btn-square" @click="closeModal()">
+                        <Icon icon="carbon:close" />
+                    </button>
+                </div>
+                <div class="p-2 overflow-y-auto">
+                    <p v-html="content" class="py-4"></p>
+                </div>
             </div>
         </div>
     </Teleport>
