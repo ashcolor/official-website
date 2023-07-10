a3245fb430b384af415aa3e625f9688a6f335cd4
ashcolor <ashcolor27@gmail.com>
2023-07-10 22:32:54 +0900

    del: gamerページを削除

--- layouts/gamer.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div>
-        <GamerHeader />
-        <div class="container max-w-8xl mx-auto py-6 px-4 sm:px-6 md:px-8">
-            <slot />
-        </div>
-        <CommonFooter />
-    </div>
-</template>
