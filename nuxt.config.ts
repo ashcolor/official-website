6878d984367be1b18f4a5a31f3e67a88b675fbec
ashcolor <ashcolor27@gmail.com>
2023-11-13 13:01:46 +0900

    add: 等幅フォント追加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -38,6 +38,7 @@ export default defineNuxtConfig({
     googleFonts: {
         families: {
             "Noto+Sans+JP": true,
+            "Noto+Sans+Mono+CJK+JP": true,
             Comfortaa: true,
         },
     },
