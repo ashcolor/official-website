cd4da066686c9351ae0246ca531e7fa78d5a000d
ashcolor <ashcolor27@gmail.com>
2024-02-25 21:46:55 +0900

    chg: header整理

--- app.vue
+++ app.vue
@@ -1,5 +1,18 @@
 <script setup lang="ts">
+const route = useRoute();
+
+const url = `${import.meta.env.VITE_NUXT_PUBLIC_SITE_URL}${route.path}`;
+
 const siteTitle = "ashcolor official website";
+
+useSeoMeta({
+    ogImage: "/img/artist.png",
+    ogUrl: url,
+    ogType: "article",
+    ogSiteName: SITE_TITLE,
+    twitterCard: "summary_large_image",
+});
+
 useHead({
     titleTemplate: (titleChunk) => {
         return titleChunk ? `${titleChunk} - ${siteTitle}` : `${siteTitle}`;
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,4 @@
-<script setup lang="ts">
-const route = useRoute();
-
-const url = `${import.meta.env.VITE_NUXT_PUBLIC_SITE_URL}${route.path}`;
-
-useSeoMeta({
-    ogImage: "/img/artist.png",
-    ogUrl: url,
-    ogType: "article",
-    ogSiteName: SITE_TITLE,
-    twitterCard: "summary_large_image",
-});
-</script>
+<script setup lang="ts"></script>
 
 <template>
     <div>
